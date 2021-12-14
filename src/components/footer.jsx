cc17ccfce2465b118dd05ebc935635036659d318
Sergio Inzunza <sinzunza@sdgku.edu>
2021-12-13 21:44:24 -0700

    FSDI class 1

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h4>Organika Store 2021. All rights reserved</h4>
+      <h5>Sergio Inzunza</h5>
+    </div>
+  );
+}
+
+export default Footer;
